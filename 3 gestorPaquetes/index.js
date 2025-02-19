9a48779775f03722bb461cc1454ab3aa2b922741
Your Name <lopezalexx1409@gmail.com>
2025-02-18 19:24:38 -0500

    Carpeta de Ejercicios

---
+++ 3 gestorPaquetes/index.js
@@ -0,0 +1,6 @@
+var superheroes = require('superheroes');
+var generateName = require('sillyname');
+var sillyName = generateName();
+
+console.log(sillyName);
+console.log(superheroes.randomSuperhero());
